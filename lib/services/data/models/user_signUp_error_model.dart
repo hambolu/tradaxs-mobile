1ef427c821ad5ba99c3a85822a63abf5e681709b
Marvel <henryadebayo250@gmail.com>
2022-10-27 23:31:57 -0700

    done with auth and error handling for auth ✅

--- lib/services/data/models/user_signUp_error_model.dart
+++ lib/services/data/models/user_signUp_error_model.dart
@@ -14,39 +14,19 @@ class UserSignUpErrorResponseModel {
   UserSignUpErrorResponseModel({
     this.status,
     this.message,
-    this.errors,
   });
 
   final bool? status;
   final String? message;
-  final Errors? errors;
 
   factory UserSignUpErrorResponseModel.fromJson(Map<String, dynamic> json) =>
       UserSignUpErrorResponseModel(
         status: json["status"],
         message: json["message"],
-        errors: Errors.fromJson(json["errors"]),
       );
 
   Map<String, dynamic> toJson() => {
         "status": status,
         "message": message,
-        "errors": errors!.toJson(),
-      };
-}
-
-class Errors {
-  Errors({
-    this.email,
-  });
-
-  final List<String>? email;
-
-  factory Errors.fromJson(Map<String, dynamic> json) => Errors(
-        email: List<String>.from(json["email"].map((x) => x)),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "email": List<dynamic>.from(email!.map((x) => x)),
       };
 }
